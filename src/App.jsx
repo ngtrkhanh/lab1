2bf00a1ce7e862adfd30d7cfc4b9bb44eef7bf03
ngtrkhanh21 <khanhntse184530@fpt.edu.vn>
2025-06-04 12:49:28 +0700

    1
    new1

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-  fetch('/MOCK_DATA.json')
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json); 
-      setData(json);
-    })
-    .catch((err) => console.error('Error loading data:', err));
-}, []);
+    fetch('/MOCK_DATA.json')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setData(json);
+      })
+      .catch((err) => console.error('Error loading data:', err));
+  }, []);
 
   // Hàm xử lý khi click "Delete"
   const handleDelete = (id) => {
@@ -23,19 +23,14 @@ function App() {
 
   return (
     <div>
-      
+
       {data.length === 0 ? (
         <p>Đang tải dữ liệu...</p>
       ) : (
         data.map((user) => (
           <div
             key={user.id}
-            style={{
-              border: '1px solid gray',
-              marginBottom: 10,
-              padding: 10 ,
-              borderRadius: 8,
-            }}
+            className="user-card"
           >
             <p><strong>Name:</strong> {user.firstName} {user.lastName}</p>
             <p><strong>Email:</strong> {user.email}</p>
@@ -44,8 +39,8 @@ function App() {
             <p><strong>Country:</strong> {user.country}</p>
 
             {user.isDelete && (
-              <button onClick={() => handleDelete(user.id)} style={{ color: 'black', backgroundColor: 'white', border: 'none', padding: '5px 10px', borderRadius: 4 }}>
-                 Delete
+              <button className="delete-btn" onClick={() => handleDelete(user.id)}>
+                Delete
               </button>
             )}
           </div>
